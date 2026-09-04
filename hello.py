65e438aa64830be413b98df2caadfd8c177875ff
Naim <naimzakirhadja@gmail.com>
2026-09-04 11:14:04 +0800

    Add print Age

--- hello.py
+++ hello.py
@@ -3,10 +3,44 @@ print("Welcome here")
 print("My first post!")
 print("===========================")
 
-username = "cool_creator"
-bio = "Fun Blogger"
+username = input("Enter Username: ")
+age = int(input("Enter Age: "))
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("=====================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
+
+if age>40 and category == "fun":
+    print("You are old what is fun for you??")
+
+bio = input("Blogger info: ")
 followers = 100
+
+print("\nInstagram Profile")
+print("=====================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
+
+
+
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
+
 
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
+
+
+
