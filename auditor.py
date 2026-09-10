b83a878540a5ba1acfe5704eb8acd7d2d728a6c3
Naim <naimzakirhadja@gmail.com>
2026-09-11 00:35:13 +0800

    Keep total of the inventory, break look immediately exceeds 500 units, Reporting

--- auditor.py
+++ auditor.py
@@ -1,6 +1,6 @@
-inventory1 = 0
-
-while int(inventory1) <= 100:
+inventory = 0
+fail = 0
+while inventory <= 500:
     print("type 'quit' to quit")
     inventory1 = input("Please enter a stock quantity: ")
     if str(inventory1).isdigit() == False:
@@ -8,12 +8,17 @@ while int(inventory1) <= 100:
            break
        else:
         print("Rejected, positive numbers only")
+        fail+=1
     else:
         if int(inventory1) < 0:
             print("Rejected negative numbers, positive numbers only")
+            fail+=1
         else:
-            print(inventory1)
+            inventory = int(inventory1)
+    print(inventory)
 
-
-
-
+if int(inventory) == 500:
+    print("Total inventory exceeds 500 units")
+else:
+    print("Total Units Processed "+str(inventory))
+print("Number of Failed/Rejected Entries "+str(fail))
