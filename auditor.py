95ba53e98862e90ac52c6a26e9cd0c15ddecb432
Naim <naimzakirhadja@gmail.com>
2026-09-11 00:31:16 +0800

    Function to reject string and negative number

--- auditor.py
+++ auditor.py
@@ -1,8 +1,19 @@
 inventory1 = 0
+
 while int(inventory1) <= 100:
+    print("type 'quit' to quit")
     inventory1 = input("Please enter a stock quantity: ")
     if str(inventory1).isdigit() == False:
        if str(inventory1) ==  "quit":
            break
-    print(inventory1)
+       else:
+        print("Rejected, positive numbers only")
+    else:
+        if int(inventory1) < 0:
+            print("Rejected negative numbers, positive numbers only")
+        else:
+            print(inventory1)
+
+
+
 
